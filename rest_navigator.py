241c20f6f1bedbd377f1e8d762633410062fa0d4
Josh Kuhn <deontologician@gmail.com>
2013-04-16 23:34:00 -0400

    Added more functionality and tests

--- rest_navigator.py
+++ rest_navigator.py
@@ -16,21 +16,50 @@ def autofetch(fn):
             self.GET()
         return fn(self, *args, **kwargs)
     return wrapped
-    
+
+
+class WileECoyoteException(ValueError):
+    '''Raised when a url has a bad scheme'''
+    pass
+
+
+class ZachMorrisException(ValueError):
+    '''Raised when a url has too many schemes'''
+    pass
+
+
+def fix_scheme(url):
+    '''Appends the http:// scheme if necessary to a url. Fails if a scheme 
+    other than http is used'''
+    splitted = url.split('://')
+    if len(splitted) == 2:
+        if splitted[0] == 'http':
+            return url
+        else:
+            raise WileECoyoteException(
+                'Bad scheme! Got: {}, expected http'.format(splitted[0]))
+    elif len(splitted) == 1:
+        return 'http://' + url
+    else:
+        raise ZachMorrisException('Too many schemes!')
+
 
 class Navigator(object):
     '''The main navigation entity'''
     
     def __init__(self, root, name=None):
-        self.root = root
+        self.root = fix_scheme(root)
         self.name = root if name is None else name
-        self.url = root
+        self.url = self.root
         self.profile = None
         self.title = None
         self.type = 'application/hal+json'
         self.response = None
+        self.state = None
 
-        self._links = None        
+        self._links = None
+        #This is the identity map shared by all descendents of this Navigator
+        self._id_map = {self.root: self}
 
     def __repr__(self):
         return "Navigator('{.name}')".format(self)
@@ -44,20 +73,33 @@ class Navigator(object):
     def GET(self):
         r'''Handles GET requests for a resource'''
         self.response = requests.get(self.url)
+        body = self.response.json()
         self._links = {rel: self._copy(url=link['href'],
                                        rel=rel,
                                        title=link.get('title'),
                                        type=link.get('type'),
                                        profile=link.get('profile')
                                    )
-                       for rel, link in self.response.json()['_links'].iteritems()}
+                       for rel, link in body.get('_links', {}).iteritems()
+                       if rel != 'self'}
+        self.title = body.get('_links', {}).get('self', {}).get('title', self.title)
+        self.state = {k:v for k,v in self.response.json().iteritems()
+                      if k not in ('_links', '_embedded')}
+        self.state.pop('_links', None)
+        self.state.pop('_embedded', None)
 
     def _copy(self, **kwargs):
-        '''Creates a shallow copy of the Navigator that extra attributes can be set on'''
+        '''Creates a shallow copy of the Navigator that extra attributes can be set on.
+        If the object is already in the identity map, that object is returned instead
+        '''
+        if 'url' in kwargs and kwargs['url'] in self._id_map:
+            return self._id_map[kwargs['url']]
         cp = copy.copy(self)
         cp._links = None
         cp.response = None
         for attr, val in kwargs.iteritems():
+            if attr == 'url':
+                self._id_map[val] = cp
             if val is not None:
                 setattr(cp, attr, val)
         return cp
@@ -69,3 +111,7 @@ class Navigator(object):
 
     def __eq__(self, other):
         return self.url == other.url and self.name == other.name
+
+    @autofetch
+    def __call__(self):
+        return self.state.copy()
--- tests/test_rest_navigator.py
+++ tests/test_rest_navigator.py
@@ -24,35 +24,101 @@ def httprettify():
         HTTPretty.disable()
 
 
-@pytest.fixture()
-def requests(monkeypatch):
-    m = mock.create_autospec(RN.requests)
-    monkeypatch.setattr(RN, 'requests', m)
-    return m
+def register_hal(url, links=None, state=None, title=None, method='GET'):
+    '''Convenience function that registers a hal document at a given address'''
+    links = links.copy() if links is not None else {}
+    state = state.copy() if state is not None else {}
+    links.update({'self': {'href': url}})
+    if title is not None:
+        links['self']['title'] = title
+    state.update({'_links': links})
+    HTTPretty.register_uri(method=method,
+                           body=json.dumps(state),
+                           content_type='application/hal+json',
+                           uri=url)
+
+
+def test_fix_scheme():
+    assert RN.fix_scheme('http://www.example.com') == 'http://www.example.com'
+    assert RN.fix_scheme('www.example.com') == 'http://www.example.com'
+    with pytest.raises(ValueError):
+        RN.fix_scheme('ftp://www.example.com')
+    with pytest.raises(ValueError):
+        RN.fix_scheme('http://http://www.example.com')
+
 
 def test_Navigator__creation():
-    N = RN.Navigator('http://haltalk.herokuapp.com')
+    N = RN.Navigator('http://www.example.com')
     assert type(N) == RN.Navigator
-    assert repr(N) == "Navigator('http://haltalk.herokuapp.com')"
+    assert repr(N) == "Navigator('http://www.example.com')"
 
 
 def test_Navigator__optional_name():
-    N = RN.Navigator('http://haltalk.herokuapp.com', name='haltalk')
-    assert repr(N) == "Navigator('haltalk')"
+    N = RN.Navigator('http://www.example.com', name='exampleAPI')
+    assert repr(N) == "Navigator('exampleAPI')"
 
 
 def test_Navigator__links():
-    with httprettify() as HTTPretty:
-        HTTPretty.register_uri(HTTPretty.GET, 'http://haltalk.herokuapp.com/',
-                               body=json.dumps({
-                                   '_links': {
-                                       'ht:users': {
-                                           'href': 'http://haltalk.herokuapp.com/users'
-                                       }
-                                   }
-                               }))
-    
-        N = RN.Navigator('http://haltalk.herokuapp.com')
+    with httprettify():
+        register_hal('http://www.example.com/',
+                     links={'ht:users': {'href': 'http://www.example.com/users'}})
+        N = RN.Navigator('http://www.example.com')
         assert N.links == {'ht:users':
-                           RN.Navigator('http://haltalk.herokuapp.com')['ht:users']}
+                           RN.Navigator('http://www.example.com')['ht:users']}
     
+def test_Navigator__call():
+    with httprettify() as HTTPretty:
+        url = 'http://www.example.com/index'
+        server_state = dict(some_attribute='some value')
+        register_hal(url=url, state=server_state, title='Example Title')
+
+        N = RN.Navigator(url)
+        assert N.state is None
+        assert N() == server_state
+        assert N.state == N()
+        assert N.state is not N()
+        assert N() is not N()
+
+def test_Navigator__init_accept_schemaless():
+    url = 'www.example.com'
+    N = RN.Navigator(url)
+    assert N.url == 'http://' + url
+    url2 = 'http://example.com'
+    N2 = RN.Navigator(url2)
+    assert N2.url == url2
+
+def test_Navigator__getitem_self_link():
+    with httprettify() as HTTPretty:
+        url = 'http://www.example.com/index'
+        title = 'Some kinda title'
+        register_hal(url, title=title)
+
+        N = RN.Navigator(url)
+        N()  # fetch it
+        assert N.title == title
+
+
+def test_Navigator__identity_map():
+    with httprettify() as HTTPretty:
+        index_url = 'http://www.example.com/'
+        page1_url = index_url + '1'
+        page2_url = index_url + '2'
+        page3_url = index_url + '3'
+        index_links = {'first': {'href': page1_url}}
+        page1_links = {'next': {'href': page2_url}}
+        page2_links = {'next': {'href': page3_url}}
+        page3_links = {'next': {'href': page1_url}}
+        
+        register_hal(index_url, index_links)
+        register_hal(page1_url, page1_links)
+        register_hal(page2_url, page2_links)
+        register_hal(page3_url, page3_links)
+
+        N = RN.Navigator(index_url)
+        page1 = N['first']
+        page2 = N['first']['next']
+        page3 = N['first']['next']['next']
+        page4 = N['first']['next']['next']['next']
+        assert page1 is page4
+        assert page2 is page4['next']
+        assert page3 is page4['next']['next']
