2cbb209d874bfceea4db0a3227f041d348c4e9ad
Josh Kuhn <deontologician@gmail.com>
2013-05-25 22:15:52 -0400

    Fixing up testing dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name="rest_navigator",
@@ -9,7 +25,15 @@ setup(
     packages=find_packages(),
     url="https://www.github.com/deontologician/rest_navigator",
     install_requires=["requests==1.1.0",
-                      "uritemplate==0.5.2",
-                      "httpretty==0.6.0"
-                     ]
+                      "uritemplate",
+                      ],
+    tests_require=[
+        "httpretty==0.6.0",
+        "pytest==2.3.4",
+    ],
+    cmdclass={'test': PyTest},
+    dependency_links=[
+        'https://github.com/uri-templates/uritemplate-py/tarball/master'
+        '#egg=uritemplate-0.5.3',
+    ],
 )
